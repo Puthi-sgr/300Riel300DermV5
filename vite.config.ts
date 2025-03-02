b08d67fa5af690c0b6ed1d8614f9555a043a1406
Puthi-sgr <chethaputhi@gmail.com>
2025-03-02 11:52:22 +0700

    Update the website domain name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/300Riel300DermV4/",
+  base: "/300Riel300DermV5/",
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
